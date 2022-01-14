694e548b3d8d79e0400a69987165afa6c56315bd
ndar123 <samwelelsa@gmail.com>
2022-01-14 18:28:41 +0300

    crud

--- my-reactprojo/src/App.js
+++ my-reactprojo/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState} from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks]= useState([{
+    id: 1,
+    text:'Doctors Appointment',
+    day: 'Febr 5th at 2:30pm',
+    Reminder: true,},
+    {
+        id: 2,
+        text:' Meeting at School',
+        day: 'Febr 6th at 2:50pm',
+        Reminder: true,},
+        {
+            id: 3,
+            text:'Food Shopping',
+            day: 'Febr 7th at 1:30pm',
+            Reminder: false,}
+])
+const deleteTask =(id) =>{
+  setTasks(tasks.filter((task)=> task.id !== id))
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      { tasks.length > 0 ? (<Tasks  tasks={tasks} onDelete ={ deleteTask } />) : (' No Task')}
+      {/* <Tasks  tasks={tasks} onDelete ={ deleteTask}/> */}
     </div>
   );
 }
